9ccd6dcc5720efb6adddaee282dba1b0ca66c1fa
xxx <a.salapanov@g.nsu.ru>
2026-02-04 10:07:12 +0700

    1_lab

---
+++ 1_lab/main.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <numbers>
+
+#ifdef USE_DOUBLE
+using T = double;
+#else
+using T = float;
+#endif
+
+int main() {
+    const int N = 10000000;
+
+    std::vector<T> a(N);
+    T sum = 0;
+
+    const double PI = std::numbers::pi; 
+
+    for (int i = 0; i < N; i++) {
+        double x = 2.0 * PI * i / N;
+        a[i] = (T)std::sin(x);
+        sum += a[i];
+    }
+
+    std::cout << "sum = " << sum << std::endl;
+    return 0;
+}
